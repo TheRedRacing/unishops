b28537347716dd7d5ed5fda262fd8ee9bbb41394
Maxime Sickenberg~ <maxime.sickenberg@gmail.com>
2024-08-27 14:13:46 +0200

    chore: Add getOneShop, getUniShops (Debug), getLogs, and getOneLog functions

--- src/lib/apiCall.tsx
+++ src/lib/apiCall.tsx
@@ -4,8 +4,8 @@ import { db } from "@/server/db";
 
 import type { Shop } from "@prisma/client";
 
-const session = await getServerAuthSession();
 const getShops = async () => {
+    const session = await getServerAuthSession();
     const shops: Shop[] = await db.shop.findMany({
         where: {
             userId: session?.user.id,
@@ -17,4 +17,53 @@ const getShops = async () => {
     return shops;
 };
 
-export { getShops};
+const getOneShop = async (id: string) => {
+    const session = await getServerAuthSession();
+    const shop = await db.shop.findFirst({
+        where: {
+            id,
+            userId: session?.user.id,
+        },
+    });
+    return shop;
+};
+
+const getUniShops = async () => {
+    const session = await getServerAuthSession();
+    const shop = await db.shop.findFirst({
+        where: {
+            name: "UniShops",
+            userId: session?.user.id,
+        },
+        orderBy: {
+            createdAt: "desc",
+        },
+    });
+    return shop;
+}
+
+const getLogs = async () => {
+    const session = await getServerAuthSession();
+    const logs = await db.log.findMany({
+        where: {
+            userId: session?.user.id,
+        },
+        orderBy: {
+            createdAt: "desc",
+        },
+    });
+    return logs;
+};
+
+const getOneLog = async (id: string) => {
+    const session = await getServerAuthSession();
+    const log = await db.log.findFirst({
+        where: {
+            id,
+            userId: session?.user.id,
+        },
+    });
+    return log;
+};
+
+export { getShops, getOneShop, getUniShops, getLogs, getOneLog };
